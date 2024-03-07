613505aac35413663964a9d305ff89e195c5616e
WaiYanMoeMyintt <wymoemyint4523@gmail.com>
2024-03-07 22:24:03 +0630

    Add sidebarmenu data

---
+++ src/data/sidebarData.js
@@ -0,0 +1,22 @@
+import shopping from "../assets/p13.png"
+import plan from "../assets/p17.png";
+import gym from "../assets/p20.png";
+export const sidebarData = [
+   {
+    id:1,
+    title:"Go to gym",
+    path:"/go-to-gym",
+    src:gym
+
+   },{
+    id:2,
+    title:"Shopping",
+    path:"/shopping",
+    src:shopping
+   },{
+    id:3,
+    title:"Make Plan",
+    path:"/make-plan",
+    src:plan
+   }
+]
